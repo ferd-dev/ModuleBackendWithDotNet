05bcf3a3a4e597adc8fc16c9b57c1fa6a3ad8f1c
Fernando <vluisfernando62@gmail.com>
2024-05-24 18:04:36 -0400

    Practic delegates

---
+++ MathOperations.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModuleBackendWithDotNet
+{
+    delegate int MathOperation(int a, int b);
+    internal class MathOperations
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        public void BasicDelegateUsage()
+        {
+            MathOperation operation = new MathOperation(Add);
+            int result = operation.Invoke(5, 3);
+            Console.WriteLine($"La suma es: {result}");
+
+            operation = new MathOperation(Subtract);
+            result = operation.Invoke(5, 3);
+            Console.WriteLine($"La resta es: {result}");
+        }
+
+        public void MulticastDelegates()
+        {
+            MathOperation operation = null;
+            operation += Add;
+            operation += Subtract;
+
+
+            Delegate[] delegateList = operation.GetInvocationList();
+            foreach (MathOperation op in delegateList)
+            {
+                int result = op.Invoke(5, 3);
+                Console.WriteLine("Result: " + result);
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿using ModuleBackendWithDotNet;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        MathOperations mathOperation = new MathOperations();
+
+        Console.WriteLine("Basic Delegate Usage");
+        mathOperation.BasicDelegateUsage();
+
+        Console.WriteLine("\nMulticast Delegate Usage:");
+        mathOperation.MulticastDelegates();
+    }
+}
