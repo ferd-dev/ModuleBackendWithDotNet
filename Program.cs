9a73c01beb07b1f81b1cfa39d2c1d67224682af0
Fernando <vluisfernando62@gmail.com>
2024-05-24 21:41:43 -0400

    Practic exception hanlding linq collections generics delegates and events

--- Program.cs
+++ Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿using ModuleBackendWithDotNet;
+
+TextProcessor textProcessor = new TextProcessor("texto.txt");
+
+// Step 2: Reading the Text File
+textProcessor.ReadTextFile();
+
+Console.WriteLine("Step 3: Word Count");
+int count = textProcessor.CountWords();
+Console.WriteLine($"Count = {count}");
+
+Console.WriteLine("\nStep 4: Character Frequency Analysis");
+var frequency = textProcessor.CalculateCharacterFrequency();
+foreach (var item in frequency) {
+    Console.Write($"{item.Key}: {item.Value} | ");
+}
+
+Console.WriteLine("\n\nStep 5: Finding the Longest Word\r");
+var longestWord = textProcessor.FindLongestWord();
+Console.WriteLine($"Long Word = {longestWord}");
+
+
---
+++ TextProcessor.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModuleBackendWithDotNet
+{
+    internal class TextProcessor
+    {
+        private string _filePath;
+        private string _text;
+        public TextProcessor(string filePath)
+        {
+            _filePath = filePath;
+            string text = ReadTextFile();
+            _text = text;
+        }
+        public string ReadTextFile()
+        {
+            try
+            {
+                return File.ReadAllText("../../../" + _filePath);
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("El archivo no fue encontrado.");
+                return "";
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ocurrió un error: {ex.Message}");
+                return "";
+            }
+        }
+
+        public int CountWords()
+        {
+            if (string.IsNullOrWhiteSpace(_text))
+            {
+                return 0;
+            }
+
+            var words = _text.Split(new[] { ' ', '\n', '\r', '\t', '.', ',', ';', '!', '?' }, StringSplitOptions.RemoveEmptyEntries);
+            return words.Length;
+        }
+
+        public Dictionary<char, int> CalculateCharacterFrequency()
+        {
+            return _text.Where(char.IsLetterOrDigit)
+                    .GroupBy(c => c)
+                    .ToDictionary(g => g.Key, g => g.Count());
+        }
+
+        public string FindLongestWord()
+        {
+            var words = _text.Split(new[] { ' ', '\n', '\r', '\t', '.', ',', ';', '!', '?' }, StringSplitOptions.RemoveEmptyEntries);
+            return words.OrderByDescending(word => word.Length).FirstOrDefault();
+        }
+    }
+}
